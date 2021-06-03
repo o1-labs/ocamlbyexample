fe9ad329fa9c0a4ded3fa71ca1ca26b2f671e486
David Wong <davidwong.crypto@gmail.com>
2021-06-02 19:16:17 -0700

    [qcheck] better example

--- book/chapters/build/qcheck/tests.ml
+++ book/chapters/build/qcheck/tests.ml
@@ -5,25 +5,23 @@ let encode_decode =
         let hexstring = Bytes.of_string some_string |> Hexstring.encode in
         Hexstring.decode hexstring = Ok expected_result))
 
-let arbitrary_hexstring =
+let elem_gen = 
   let open QCheck.Gen in
-  (* the hexadecimal range *)
   let c = char_range 'a' 'f' in
   let d = char_range '0' '9' in
-  (* randomly choose a character in any given range *)
-  let elem_gen = oneof [ c; d ] in
-  (* an hexstring is of even size ( 2 x natural number ) *)
-  let size_gen = map (fun x -> 2 * x) nat in
-  (* creates lists of size [size_gen] *)
-  (* containing elements generated via [elem_gen] *)
-  list_size size_gen elem_gen
+  oneof [ c; d ]
+
+let size_gen = 
+  QCheck.Gen.(map (fun x -> 2 * x) nat)
+
+let arbitrary_hexstring =
+  QCheck.Gen.list_size size_gen elem_gen
 
 let decode_arbitrary_hexstring =
   QCheck.(
     Test.make ~count:1000 ~name:"rand hexstring -> decode"
       (make arbitrary_hexstring) (fun arb ->
         let s = arb |> List.to_seq |> String.of_seq in
-        let len = List.length arb in
         let decoded = Hexstring.decode s in
         match decoded with
         | Ok r -> Hexstring.encode r = s
