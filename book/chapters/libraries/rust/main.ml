1962a480f33a20d7a218eee0b962dd10866e09db
David Wong <davidwong.crypto@gmail.com>
2021-06-02 21:37:02 -0700

    [rust] better explanations

--- book/chapters/libraries/rust/main.ml
+++ book/chapters/libraries/rust/main.ml
@@ -1,4 +1,16 @@
-open Lib
+type house = {
+  name: string;
+  windows: int;
+  door: bool;
+  cupboards: int64;
+}
+
+
+external build : string -> int -> house = "build_house"
+
+external destroy : house -> house = "destroy_house"
+
+external display : house -> unit = "display_house"
 
 let () = 
   let h = build "casa" 5 in
